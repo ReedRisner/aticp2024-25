808258c2f1c72d88fbc2983871fab10530c68fd9
ReedRisner <rrisner@micds.org>
2024-09-19 11:35:19 -0500

    Working on our PA03

--- PA03/PA03.cpp
+++ PA03/PA03.cpp
@@ -6,4 +6,18 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
+    if(argc == 1){
+        cerr <<"Only One Argument"<<endl;
+        exit(1);
+    } 
+
+    string fileName = argv[1];
+    ifstream infile (fileName);
+    string line;
+    
+    if (infile.good() == false) {
+        cerr << "Unable to open the file named " << fileName;
+        exit (1);
+    }
+
 }
