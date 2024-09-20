0d29cb94d6866052beb6cd7fecd4dec9a4f47b93
ReedRisner <rrisner@micds.org>
2024-09-19 22:30:49 -0500

    Still Working

--- PA03/PA03.cpp
+++ PA03/PA03.cpp
@@ -2,39 +2,50 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
-
+#include <string>
 
 using namespace std;
 
+char calculateGrade(int score) {
+    if (score >= 90) return 'A';
+    if (score >= 80) return 'B';
+    if (score >= 70) return 'C';
+    if (score >= 60) return 'D';
+    return 'F';
+}
+void split(const string& str, const string& delim, vector<string>& parts) {
+  size_t start, end = 0;
+  while (end < str.size()) {
+    start = end;
+    while (start < str.size() && (delim.find(str[start]) != string::npos)) {
+      start++;  // skip initial whitespace
+    }
+    end = start;
+    while (end < str.size() && (delim.find(str[end]) == string::npos)) {
+      end++; // skip to end of word
+    }
+    if (end-start != 0) {  // just ignore zero-length strings.
+      parts.push_back(string(str, start, end-start));
+    }
+  }
+}  
 int main(int argc, char const *argv[]) {
 
-struct Student {
-    string firstname;
-    string lastname;
-    int score;
-    char grade;
-};
-
-vector<student>;
-
+string inputName = argv[1];
+string outputName = argv[2];
+ifstream infile (inputName);
+ofstream outfile (outputName);
+string line;
+   
     if(argc != 3){
         cerr <<"Not enough arguments"<<endl;
         exit(1);
-    } 
-
-    string inputName = argv[1];
-    string outputName = argv[2];
-    ifstream infile (inputName);
-    ofstream outfile (outputName);
-    string line;
-    
+    };
+     
     if (infile.good() == false || outfile.good() == false) {
         cerr << "Unable to open the file named " << inputName;
         exit (1);
     }
-
-
-
-
+    
 
 }
