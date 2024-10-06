4e6a4d4651f5ca5f7024870da0864f832d9b6782
ReedRisner <rrisner@micds.org>
2024-10-06 14:36:20 -0500

    Start PA05

---
+++ PA05/convert.cpp
@@ -0,0 +1,44 @@
+// You will need to edit this file and turn this one in.
+
+#include <iostream>
+#include <string>
+
+// char2int
+// Converts from a character to an integer digit
+// if character '0'..'9' convert to 0..9
+// else if character 'A'..'F' convert to 10..15
+// else convert to -1
+int char2int(char digit)
+{
+   // in ASCII code the numbers start from 48. 
+   
+    
+
+
+
+
+}
+
+// int2char
+// Converts from an integer digit to a character
+// if integer 0..9 convert to '0'..'9'
+// else if integer 10..15 convert to 'A'..'F'
+// else convert to 'X'
+char int2char(int digit)
+{
+    return 0;
+}
+
+// Convert integer to string in specified base and print
+// 2 <= base <= 16
+void int2ascii(int value, int base)
+{
+    return;
+}
+
+// Convert string in specified base to integer and print
+// 2 <= base <= 16
+void ascii2int(const std::string &ascii, int base)
+{
+    return;
+}
