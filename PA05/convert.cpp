86cdad471b16959a02103c31965d3e37baa55072
ReedRisner <rrisner@micds.org>
2024-10-07 09:46:59 -0500

    Almost Done with PA05

--- PA05/convert.cpp
+++ PA05/convert.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <sstream>
 
+using namespace std;
+
 // char2int
 // Converts from a character to an integer digit
 // if character '0'..'9' convert to 0..9
@@ -54,8 +56,22 @@ char int2char(int digit)
 
 // Convert integer to string in specified base and print
 // 2 <= base <= 16
-void int2ascii(int value, int base)
-{
+void int2ascii(int value, int base) {
+    if (base == 10) {
+        cout<<value<<endl;
+        return;
+    }
+
+   vector <int> remains;
+
+    for (int x = 0; value > 0; x++) {
+        remains.push_back(value%base);
+        value/=base;
+    }
+    for (int x = remains.size()-1; x>=0; x--) {
+        int digit  = remains.at(1);
+        std::cout<<int2char(digit);
+    }
     return;
 }
 
@@ -63,5 +79,16 @@ void int2ascii(int value, int base)
 // 2 <= base <= 16
 void ascii2int(const std::string &ascii, int base)
 {
+    int place = 0;
+    int nv = 0;
+    int value = ascii.length();
+    for (int i = 0; value >0; i++) {
+        string placestring = ascii.substr(value-1, 1);
+        char Char = placestring[0];
+        place = char2int(Char);
+        nv += place * pow(base, i);
+        value--;
+    }
+    cout<<nv<<endl;
     return;
 }
