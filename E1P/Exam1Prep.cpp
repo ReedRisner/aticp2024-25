dbb93a868c7c846d6ba10990e0c5e2ef1408c2c0
ReedRisner <rrisner@micds.org>
2024-11-06 08:56:59 -0600

    Exam 1 Prep

---
+++ E1P/Exam1Prep.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+/*
+ * Complete the 'findModeCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER num
+ *  2. INTEGER base
+ *  3. STRING start
+ */
+
+int findModeCount(int num, int base, string start) {
+    return 0; //Change this to return the correct answer.
+}
+
+
